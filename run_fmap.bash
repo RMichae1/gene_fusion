58c134417652de622756901946dd80949ee223ef
Richard Michael <richard.michael@zapliance.de>
2020-02-25 10:08:47 +0100

    parsing fastq reads from benchmarking into the fmap run script

--- run_fmap.bash
+++ run_fmap.bash
@@ -8,13 +8,30 @@ FMAP_SCRIPT="${BASE_DIR}/gene_fusion/FusionMap.sh"
 FMAP_CONFIG="${BASE_DIR}/gene_fusion/FusionMap_options.txt"
 OUTPUT_DIR="${BASE_DIR}/output/"
 
+LEFT_FILES="${DATA_DIR}/synth/*/data.broadinstitute.org/Trinity/CTAT_FUSIONTRANS_BENCHMARKING/on_simulated_data/*/reads/*_1.fq*"
+RIGHT_FILES="${DATA_DIR}/synth/*/data.broadinstitute.org/Trinity/CTAT_FUSIONTRANS_BENCHMARKING/on_simulated_data/*/reads/*_2.fq*"
+
 function run_fmap() {
-    ${FMAP_SCRIPT} -c ${FMAP_CONFIG} -o ${OUTPUT_DIR} -t 4
+    ${FMAP_SCRIPT} -c ${FMAP_CONFIG} -o ${SAMPLE_OUTPUT_DIR} -t 4 ${LEFT_FASTQ} ${RIGHT_FASTQ}
 }
 
-echo "Running FusionMap from ${FMAP_CONFIG} ..."
-echo "Writing output to ${OUTPUT_DIR}"
+for left in ${LEFT_FILES}; do
+   for right in ${RIGHT_FILES}; do
+      if [[ ${left%_*} == ${right%_*} ]]; then
+        BASENAME=$(basename ${left})
+        SAMPLE_OUTPUT_DIR=${OUTPUT_DIR}/${BASENAME%_*}
+        LEFT_FASTQ=${left}
+        RIGHT_FASTQ=${right}
+        
+        echo "Running FusionMap on ${BASENAME}..."
+        echo "Mapping ${LEFT_FASTQ} with ${RIGHT_FASTQ}..."
+        echo "Writing output to ${SAMPLE_OUTPUT_DIR}"
+        run_fmap
+        
+      fi
+    done
+done
 
-run_fmap
+
 
 exit 0
