033a9f0c7044b70762feb03dd36767feb519dc84
Richard Michael <pcq275@alumni.ku.dk>
2020-02-25 09:17:46 +0100

    added cleaning routine for Chimeric junction file to prevent conversion errors in STAR-SEQR

--- run_seqr.bash
+++ run_seqr.bash
@@ -18,7 +18,7 @@ OUTPUT_DIR="${BASE_DIR}/output/"
 # build index with STAR first
 
 function star_seqr(){
-starseqr.py -1 ${LEFT_FASTQ} -2 ${RIGHT_FASTQ} -m 1 -p RNA_${BASENAME%_*} -t 4 -i ${GENOME_LIB}/ref_genome.fa.star.idx -g ${GENOME_LIB}/ref_annot.gtf -r ${GENOME_LIB}/ref_genome.fa -vv
+starseqr.py -1 ${LEFT_FASTQ} -2 ${RIGHT_FASTQ} -m 1 -p RNA_${BASENAME%_*} -t 4 -sb ${BAM_STAR_FILE} -sj ${JUNCT_STAR_FILE} -g ${GENOME_LIB}/ref_annot.gtf -r ${GENOME_LIB}/ref_genome.fa -vv
 }
 
 for left in ${LEFT_FILES}; do
@@ -26,13 +26,33 @@ for left in ${LEFT_FILES}; do
       if [[ ${left%_*} == ${right%_*} ]]; then
        BASENAME=$(basename ${left})
        SAMPLE_OUTPUT_DIR=${OUTPUT_DIR}/${BASENAME%_*}/star_seqr
+       JUNCT_STAR_FILE=${BASE_DIR}/output/${BASENAME%_*}/Chimeric.out.junction
+       BAM_STAR_FILE=${BASE_DIR}/output/${BASENAME%_*}/Aligned.out.bam
+
         # check if output directory exists and create
          if [[ ! -d ${SAMPLE_OUTPUT_DIR} ]]; then
                 echo "creating dir ${SAMPLE_OUTPUT_DIR}"
                 mkdir ${SAMPLE_OUTPUT_DIR}
          fi
-         # JUNCT_STAR_FILE=${BASE_DIR}/output/${BASENAME%_*}/Chimeric.out.junction
-         # BAM_STAR_FILE=${BASE_DIR}/output/${BASENAME%_*}/Aligned.out.bam
+
+	# STAR-SEQR cannot handle header or commenting tail lines => clean files
+	HEADER_FIRST_ELEM="chr_donorA"
+	FOOTER_ELEM="#"
+	FILE_FIRST_ELEM=$(head -n 1 ${JUNCT_STAR_FILE} | awk -F '\t' '{print $1}')
+	FILE_FOOTER_ELEM=$(tail -n 1 ${JUNCT_STAR_FILE} | awk -F '\t' '${print substr($1,1,1)}')
+	# We are going to change the file around.. lets make a backup first
+	if [[${HEADER_FIRST_ELEM} == ${FILE_FIRST_ELEM} ]] || [[ ${FOOTER_ELEM} == ${FILE_FOOTER_ELEM} ]]; then
+		cp ${JUNCT_STAR_FILE} ${JUNCT_STAR_FILE}.bak
+	fi
+	# use first value to test if header exists
+	 if [[ ${HEADER_FIRST_ELEM} == ${FILE_FIRST_ELEM} ]]; then
+		# clean header
+		sed 1,1d ${JUNCT_STAR_FILE} > ${JUNCT_STAR_FILE}	
+	 fi
+	 if [[ ${FOOTER_ELEM} == ${FILE_FOOTER_ELEM} ]]; then
+		# clean tail commenting lines
+		head -n -1 ${JUNCT_STAR_FILE} > ${JUNCT_STAR_FILE}
+	 fi
          LEFT_FASTQ=${left}
          RIGHT_FASTQ=${right}
          echo "Run SEQR on ${BASENAME} -> output: ${SAMPLE_OUTPUT_DIR}"
