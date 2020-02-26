3ea7593d772037e0ea24afee759bb099fc67ea94
Richard Michael <richard.michael@zapliance.de>
2020-02-26 10:26:49 +0100

    added correct paths for data

--- run_fmap.bash
+++ run_fmap.bash
@@ -2,6 +2,7 @@ module load tools
 module load oshell/10.0.1.69
 
 BASE_DIR="/home/projects/cu_10160/people/ricmic/"
+DATA_DIR="${BASE_DIR}/data/"
 FMAP_SCRIPT="${BASE_DIR}/gene_fusion/FusionMap.sh"
 FMAP_CONFIG="${BASE_DIR}/gene_fusion/FusionMap_options.txt"
 OUTPUT_DIR="${BASE_DIR}/output/"
