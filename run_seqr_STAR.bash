dbcb49036184e987fde6d51cfa38ba4e77cf55d6
Richard Michael <richard.michael@zapliance.de>
2020-03-04 08:33:15 +0100

    changed seqr naming convention for output

--- run_seqr_STAR.bash
+++ run_seqr_STAR.bash
@@ -15,23 +15,14 @@ OUTPUT_DIR="${BASE_DIR}/output/"
 # build index with STAR first
 
 function star_seqr(){
-starseqr.py -1 ${LEFT_FASTQ} -2 ${RIGHT_FASTQ} -m 1 -p SEQR -t 8 -i ${GENOME_LIB}/ref_genome.fa.star.idx/ -g ${GENOME_LIB}/ref_annot.gtf -r ${GENOME_LIB}/ref_genome.fa -vv
+starseqr.py -1 ${LEFT_FASTQ} -2 ${RIGHT_FASTQ} -m 1 -p ${BASENAME}_SEQR -t 8 -i ${GENOME_LIB}/ref_genome.fa.star.idx/ -g ${GENOME_LIB}/ref_annot.gtf -r ${GENOME_LIB}/ref_genome.fa -vv
 }
 
 
 for left in ${LEFT_FILES}; do
    for right in ${RIGHT_FILES}; do
       if [[ ${left%_*} == ${right%_*} ]]; then
-       BASENAME=$(basename ${left})
-       SAMPLE_OUTPUT_DIR=${OUTPUT_DIR}/${BASENAME%_*}/star_seqr
-       # get to the output directory
-        cd ${SAMPLE_OUTPUT_DIR}
-
-        # check if output directory exists and create
-         if [[ ! -d ${SAMPLE_OUTPUT_DIR} ]]; then
-                echo "creating dir ${SAMPLE_OUTPUT_DIR}"
-                mkdir ${SAMPLE_OUTPUT_DIR}
-         fi
+         BASENAME=$(basename ${left})
 
          LEFT_FASTQ=${left}
          RIGHT_FASTQ=${right}
