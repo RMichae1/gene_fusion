509ae128e6fadb74386052eb4683304785d903c9
Richard Michael <pcq275@alumni.ku.dk>
2020-03-23 15:04:27 +0100

    added bowtie index

---
+++ index_bowtie.bash
@@ -0,0 +1,4 @@
+module load tools
+module load bowtie2/2.3.4.1
+
+bowtie2-build --large-index /home/projects/cu_10160/people/ricmic/data/ref/GRCh38_gencode_v31_CTAT_lib_Oct012019.plug-n-play/ctat_genome_lib_build_dir/ref_genome.fa /home/projects/cu_10160/people/ricmic/data/ref/GRCh38_gencode_v31_CTAT_lib_Oct012019.plug-n-play/ctat_genome_lib_build_dir/
